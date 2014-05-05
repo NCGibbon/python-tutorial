004a692ce71ca0c5d81040a45c817d1df216351e
Jan Milosh <janmilosh@gmail.com>
2014-05-04 23:57:32 -0400

    Adds map functionality

--- busroute.py
+++ busroute.py
@@ -18,23 +18,39 @@ def get_bus_data():
 
 # parse xml and print buses to console
 # if a bus is northbound and north of the office
+# if within 0.5 miles, show on google map
 def find_northbound():
     from xml.etree.ElementTree import parse
     doc = parse('rt22.xml')
+    check_time = doc.findtext('time')
     for bus in doc.findall('bus'):
         d = bus.findtext('d')
         id = bus.findtext('id')
         lat = float(bus.findtext('lat'))
+        lon = bus.findtext('lon')
         if d == 'North Bound':
-            if lat > office_lat:
-                print ('id = ', id)
-                print ('lat = ', lat)
-                print ('dir = ', d)
-                print (distance(lat, office_lat))
+            dist = distance(lat, office_lat)
+            print ('time = ', check_time)
+            print ('id = ', id)
+            # print ('lat = ', lat)
+            # print ('lon = ', lon)
+            print ('dist = ', dist)
+            if dist <= 0.5:  
+                open_map(str(lat), lon, id)
 
 # calculate distance from office to bus
 def distance(lat1, lat2):
     return 69 * abs(lat1-lat2)
+
+# open google map in web browser and show bus and office on map
+def open_map(lat, lon, id):
+    import webbrowser
+    key = 'AIzaSyDe1fq7oib8shkDokkXzJ8H1txRTLjR8k8'
+    office_location = '&markers=color:blue%7Clabel:O%7C' + str(office_lat) + ',' + str(office_lon)
+    bus_location = '&markers=color:red%7Clabel:B%7C' + lat + ',' + lon
+    url = 'http://maps.googleapis.com/maps/api/staticmap?size=400x400&maptype=roadmap' \
+            + office_location + bus_location + '&key=' + key
+    webbrowser.open(url)
 
 # check the buses every minute
 def keep_checking(minutes):
